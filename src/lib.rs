82dbbae95e7be802a27513458ccd7781893c7062
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2025-11-14 13:00:00 +0000

    To and from bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+
+#[allow(dead_code)]
 struct BARFileHeader {
     major_version: u8,
     minor_version: u8,
@@ -5,30 +8,42 @@ struct BARFileHeader {
     version_abbrev: String,
 }
 
-impl From<&[u8]> for BARFileHeader {
-    fn from(item: &[u8]) -> Self {
-        assert!(item.len() == 16);
-        let intro = str::from_utf8(&item[0..3]).expect("Not a bar file");
-        assert!(intro == "BAR");
-        let major_version = item[3];
-        let minor_version = item[4];
-        let number_of_books = item[5];
-        let version_abbrev = String::from_utf8(item[6..16].to_vec())
-            .expect("Could not read version abbrev")
+#[allow(dead_code)]
+impl BARFileHeader {
+    fn from_bytes(buf: &[u8]) -> Result<BARFileHeader, Box<dyn Error>> {
+        if buf.len() != 16 {
+            return Err("Buffer should be 16 bytes long.".into());
+        }
+        let intro = str::from_utf8(&buf[0..3])?;
+        if intro != "BAR" {
+            return Err("Not a BAR file header.".into());
+        }
+        let major_version = buf[3];
+        let minor_version = buf[4];
+        let number_of_books = buf[5];
+        let version_abbrev = str::from_utf8(&buf[6..16])?
             .trim_end_matches("\0")
             .to_string();
-        BARFileHeader {
+        Ok(BARFileHeader {
             major_version,
             minor_version,
             number_of_books,
             version_abbrev,
-        }
+        })
     }
-}
 
-pub fn to_hex_string(buf: &[u8]) -> String {
-    let result: String = buf.iter().map(|b| format!("{:02X}", b)).collect();
-    result
+    fn to_bytes(self: Self) -> Vec<u8> {
+        let mut result: Vec<u8> = Vec::new();
+        result.append(&mut "BAR".as_bytes().to_vec());
+        result.push(self.major_version);
+        result.push(self.minor_version);
+        result.push(self.number_of_books);
+        result.append(&mut self.version_abbrev.as_bytes().to_vec());
+        while result.len() < 16 {
+            result.push(b'\0');
+        }
+        result
+    }
 }
 
 #[cfg(test)]
@@ -38,23 +53,14 @@ mod tests {
     const NIV_HEADER: &str = "4241520200425A4C4942000000000000";
 
     #[test]
-    fn test_to_hex_string() {
-        let bytes: &[u8] = &[
-            0x42, 0x41, 0x52, 0x02, 0x00, 0x42, 0x5A, 0x4C, 0x49, 0x42, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00,
-        ];
-        let hex_string = to_hex_string(bytes);
-        assert_eq!(hex_string.as_str(), NIV_HEADER);
-        assert_eq!(hex_string, hex::encode_upper(bytes));
-    }
-
-    #[test]
     fn test_barfileheader() {
         let bytes = hex::decode(NIV_HEADER).expect("Covert to bytes failed.");
-        let header: BARFileHeader = bytes[..].into();
+        let header = BARFileHeader::from_bytes(&bytes).expect("Construction from bytes failed");
         assert_eq!(header.major_version, 2);
         assert_eq!(header.minor_version, 0);
         assert_eq!(header.number_of_books, 66);
         assert_eq!(header.version_abbrev.as_str(), "ZLIB");
+        let bytes_out = header.to_bytes();
+        assert_eq!(NIV_HEADER, hex::encode_upper(&bytes_out));
     }
 }
