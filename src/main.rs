7413a8153b66d8dc4845a16c8059dccb1d459a64
Chris Hamer-Hodges <chris.hhodges@gmail.com>
2025-12-02 13:47:53 +0000

    Move hex to dev dependency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bar::binarystruct::BinaryStruct;
 use bar::{BARBookIndexEntry, BARFile};
-use hex;
 use std::{env, io};
 
 use lzokay_native;
@@ -9,13 +8,8 @@ fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         let file_path = &args[1];
-        //let mut file = File::open(file_path)?;
-        //let mut header = [0; 16];
-        //let size = file.read(&mut header[..])?;
-        //assert!(size == 16);
-        //let hex_output = hex::encode_upper(header);
         let bar = BARFile::open(file_path).expect("Failed to open");
-        let hex_output = hex::encode_upper(bar.header.to_bytes());
+        let hex_output = format!("{:?}", bar.header.to_bytes());
         println!("{hex_output}");
         println!("Version {}", bar.archive_version());
         println!("{}", bar.bible_version());
