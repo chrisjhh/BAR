e4c27b584675c52e4dd8a70d5014d6df3bf35133
Chris Hamer-Hodges <chris.hhodges@gmail.com>
2025-12-08 16:33:36 +0000

    Count the sevens

--- src/main.rs
+++ src/main.rs
@@ -80,5 +80,33 @@ fn main() -> io::Result<()> {
         }
     }
 
+    for book in bar.books() {
+        for chapter in book.chapters() {
+            if let Some(chapter) = chapter {
+                let mut count = 0;
+                for (_i, verse) in chapter.verses().enumerate() {
+                    if verse.contains("seven") {
+                        count += 1;
+                        /*println!(
+                            "{} {}:{} {}",
+                            book.book_abbrev(),
+                            chapter.chapter_number(),
+                            i + 1,
+                            verse
+                        )*/
+                    }
+                }
+                if count > 5 {
+                    println!(
+                        "{} {} : {} times",
+                        book.book_abbrev(),
+                        chapter.chapter_number(),
+                        count
+                    )
+                }
+            }
+        }
+    }
+
     Ok(())
 }
